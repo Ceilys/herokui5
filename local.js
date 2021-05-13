aa995df1de6ba9d75a417760e408da1573a688db
Christophe Gandin <cgandin@ceilys.fr>
2021-05-13 15:31:16 +0200

    init commut

---
+++ local.js
@@ -0,0 +1,24 @@
+var path = require('path'), 
+    express = require('express');
+
+var app = express();
+
+// Display on good directory
+app.use('/ui5', express.static(path.join(__dirname, 'webapp')));
+app.use('/wt', express.static(path.join(__dirname, 'walkthrough')));
+
+// For other key words, go on first page
+app.get('/', function (req, res) {
+	var miniHtml = "Test program of Christophe" + "<BR>"
+		+ "/wt   check if UI5 is active" + "<BR>"
+		+ "/ui5  send to a list (list of Jerry)" + "<BR>"
+		+ "/cv   send to a dynamic CV";
+	res.send(miniHtml);
+});
+
+// Listen to port
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+	console.log("Listen on port " + port);
+	console.log("http://localhost:" + port + "/");
+});
